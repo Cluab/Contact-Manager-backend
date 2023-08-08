74a4e81d965ba660468e2df2e2c20beffeb2df30
cluab <cluab11222@gmail.com>
2023-08-08 14:28:56 +0300

    Update Register Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,34 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
 
 //@desc Register a user
 //@route Post /api/user/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Register the user' });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error('All fields are mandatory!');
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error('User already registered');
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log(user);
+  if (user) {
+    res.status(200).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error('User data is not valid');
+  }
 });
 
 //@desc Login a user
